10920c80d5307a16ccdec19295a410ee05e93bcb
Thomas Poelman <thomas.s.poelman@gmail.com>
2014-03-27 02:25:11 -0700

    profile scoping and tests added'

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Thomasbook::Application.routes.draw do
   get 'feed', to: 'statuses#index', as: :feed
   root to: "statuses#index"
 
-
+  get '/:id', to: "profiles#show"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -2,8 +2,28 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:dean).profile_name
     assert_response :success
+    assert_template "profiles/show"
   end
 
+  test "should show not found if user not found" do
+  	get :show, id: "doesn't exist"
+  	assert_response :not_found
+  end
+
+  test "variables should be assigned on successful viewing" do
+  	get :show, id: users(:dean).profile_name
+  	assert assigns(:user)
+  	assert_not_empty assigns(:statuses)
+  end
+  
+  test "only shows the correct user's statuses" do
+  	get :show, id: users(:dean).profile_name
+  	assigns(:statuses).each do |status|
+  		assert_equal users(:dean), status.user
+  	end
+  end
+
+  
 end
