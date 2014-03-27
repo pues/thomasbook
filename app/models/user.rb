c6ae0ad20a7658c246d5d6d0bbe868825ebad530
Thomas Poelman <thomas.s.poelman@gmail.com>
2014-03-26 18:12:01 -0700

    Wrote unit tests to validate user input

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+                            uniqueness: true,
+                            format: {
+                              with: /a-zA-Z0-9_-/,
+                              message: "Must be formatted correctly."
+                            }
+
   has_many :statuses
   attr_accessible :email, :password, :password_confirmation, :remember_me,
   				:first_name, :last_name, :profile_name
